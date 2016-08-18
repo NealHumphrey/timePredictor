3b37fbef599e48060109cb9a76e9b47423a79467
NealHumphrey <humphrey.neal@gmail.com>
2016-08-18 16:23:15 -0400

    Setting up more stuff to work with AWS

--- application.py
+++ application.py
@@ -2,11 +2,12 @@
 #Unlike Django and others, there's no database or other full-stack components, making it useful for quick display-based tools.
 
 #Import the stuff we need first:
-from bottle import route, run, template, view, debug
+from bottle import route, run, template, view, debug, default_app
 from app.main import sample, slice_calendar
 from datetime import datetime
 #Obviously you can import other modules here.
 
+application = default_app()
 
 #Run arbitrary code
 sample_calendar = sample()
@@ -35,5 +36,10 @@ def hello():
 
 
 #When it's all set up, run the page.
-run(host='localhost', port=8080, debug=True, reloader=True, interval=3)
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', debug=True)
+
+#Version used for running locally:
+#run(host='localhost', port=8080, debug=True, reloader=True, interval=3)
+
 #TODO - remove the debug call in production!
